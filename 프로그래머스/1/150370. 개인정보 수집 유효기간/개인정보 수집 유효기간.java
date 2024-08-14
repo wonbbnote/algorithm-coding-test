24fe4db8bcc557ff8d7de82d2bad473f75ee675d
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-08-14 13:12:43 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 74.07 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) throws ParseException {
+        int[] answer = {};
+        // 정답 리스트 정의
+        List<Integer> res = new ArrayList<>();
+        
+        // terms를 돌면서 Map에 약관종류: 유효기간을 저장
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < terms.length; i++){
+            String[] s = terms[i].split(" ");
+            map.put(s[0], Integer.valueOf(s[1]));   
+        }
+        
+        // privacies를 돌면서 약관 종류를 확인하고, 해당 약관의 달 수를 더한다.
+        // today와 비교
+        for(int i = 0; i < privacies.length; i++){
+            String[] p = privacies[i].split(" ");
+            String privacy = p[0];
+            int term = map.get(p[1]);
+            
+            // 약관 유효기간 마감일 구하기
+            SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
+            Date date = format.parse(privacy);
+            Date todate = format.parse(today);
+            
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(date);
+            cal.add(Calendar.MONTH, term);
+            String strdate = format.format(cal.getTime());
+            date = format.parse(strdate);
+            
+            
+            // today와 비교
+            if(date.compareTo(todate) <= 0) {
+                res.add(i + 1);
+            }
+            
+        }
+        
+        // 마지막 정답 리스트를 int[]로 변환
+        answer = new int[res.size()];
+        for(int i = 0; i < res.size(); i++){
+            answer[i] = res.get(i);
+        }
+        
+        return answer;
+    }
+}
