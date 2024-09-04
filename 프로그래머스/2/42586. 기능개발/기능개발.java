a299190dd32e1929204d7fa52cb28962a4ed4fe2
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-09-04 17:50:46 +0900

    [level 2] Title: 기능개발, Time: 3.68 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,38 +1,32 @@
 import java.util.*;
-class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        List<Integer> res = new ArrayList<>();
-    
-        Queue<Integer> queue = new ArrayDeque<>();
-        
-        for(int i = 0; i < progresses.length; i++){
-            int day = (100 - progresses[i]) / speeds[i];
-            int remain = (100 - progresses[i]) % speeds[i];
-            int needDay = remain == 0 ? day : day+1;
-            queue.add(needDay);
-        }
-                
-        while(!queue.isEmpty()){
-            
-            int firstNum = queue.peek();
-            int num = 0;
-            
-            while(queue.size() > 0){
-                if(queue.peek() <= firstNum){
-                    queue.poll();
-                    num++;
-                }else{
-                    break;
-                }
-            }
 
-            res.add(num);
+class Solution{
+    public int[] solution(int[] progresses, int[] speeds){
+        Queue<Integer> answer = new ArrayDeque<>(); // 😻 answer을 큐로?
+        int n = progresses.length;
+
+        // 각 작업의 배포 가능일 계산
+        int[] daysLeft = new int[n];
+        for(int i = 0; i < n; i++){
+            daysLeft[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]); // 😻 반올림이 있었군
         }
-        
-        answer = res.stream().mapToInt(Integer::intValue).toArray();
-        
-        
-        return answer;
+
+        // 배포될 작업의 수 카운트
+        int count = 0;
+        int maxDay = daysLeft[0]; // 현재 배포될 작업 중 가장 늦게 배포될 작업의 가능일
+
+        for(int i = 0; i < n; i++){
+                if(daysLeft[i] <= maxDay){ // 배포 가능일이 가장 늦은 배포일보다 빠르면
+                        count++;
+                }else{ // 배포 가능일이 가장 늦은 배포일보다 느리면, 새로 세팅
+                        answer.add(count);
+                        count = 1;
+                        maxDay = daysLeft[i];
+                }
+        }
+
+        // 반복문을 나오기 바로 직전에 count한 값은 answer에 add()로 추가하지 않기 때문에 반복문 바깥에서 추가해줘야 한다.
+        answer.add(count);
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
