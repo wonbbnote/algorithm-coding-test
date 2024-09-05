93579398b7f8bc6c8f8592b85a5db34ec8a7a9a0
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-09-05 17:04:24 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -3,14 +3,11 @@ class Solution
     public int solution(int n, int a, int b)
     {
         int answer = 0;
-        
         int round = 2;
         while(a != b){
             answer++;
             a = (a % round == 0) ? a/round : a / round + 1;
             b = (b % round == 0) ? b/round : b / round + 1;
-            System.out.println("round: "+round+", answer: "+answer);
-            System.out.println("a: "+a+", b: "+b);
         }
         
 
