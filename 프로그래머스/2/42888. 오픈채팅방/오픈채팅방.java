06867e80ef617f07e58dd3da9ab5e97197167454
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-09-08 13:31:15 +0900

    [level 2] Title: 오픈채팅방, Time: 154.74 ms, Memory: 161 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,38 +1,29 @@
-import java.util.*;
-class Solution {
-    public String[] solution(String[] record) {
-        
-        List<String> res = new ArrayList<>();
-        Map<String, String> userList = new HashMap<>();
-        
-        for(int i = 0; i < record.length; i++){
-            String[] command = record[i].split(" ");
-            
-            if(command[0].equals("Enter")){
-                userList.put(command[1], command[2]);
-                res.add("Enter "+command[1]);
-                
-            }else if(command[0].equals("Leave")){
-                res.add("Leave "+command[1]);
-                // userList.put(command[1], "");
-                
-            }else if(command[0].equals("Change")){
-                userList.put(command[1], command[2]);
-            }
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class Solution{
+    public String[] solution(String[] record){
+        HashMap<String, String> msg = new HashMap<>();
+        msg.put("Enter", "님이 들어왔습니다.");
+        msg.put("Leave", "님이 나갔습니다.");
+
+        HashMap<String, String> uid = new HashMap<>();
+
+        for(String s: record){
+                String[] cmd = s.split(" ");
+                if(cmd.length == 3){
+                        uid.put(cmd[1], cmd[2]);
+                }
         }
-        
-        
-        String[] answer = new String[res.size()];
-        for(int i = 0; i < res.size(); i++){
-            String[] cmd = res.get(i).split(" ");
-            if(cmd[0].equals("Enter")){
-                answer[i] = userList.get(cmd[1]) + "님이 들어왔습니다.";
-            }else if(cmd[0].equals("Leave")){
-                answer[i] = userList.get(cmd[1]) + "님이 나갔습니다.";
-            }
+
+        ArrayList<String> answer = new ArrayList<>();
+        for(String s: record){
+                String[] cmd = s.split(" ");
+                if(msg.containsKey(cmd[0])){
+                        answer.add(uid.get(cmd[1]) + msg.get(cmd[0]));
+                }
         }
-        
-        
-        return answer;
+
+        return answer.toArray(new String[0]);
     }
 }
