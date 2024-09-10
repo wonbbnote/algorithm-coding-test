570c3102c1a303f12c80363a07c5577fcef886b4
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-09-10 23:24:45 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 27.30 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    
+    private static HashMap<Integer, HashMap<String, Integer>> courseMap;
+    
+    public String[] solution(String[] orders, int[] course) {
+        courseMap = new HashMap<>();
+        for(int i : course){
+            courseMap.put(i, new HashMap<>());
+        }
+        
+        for(String order: orders){
+            char[] orderArray = order.toCharArray();
+            Arrays.sort(orderArray);
+            combinations(0, orderArray, "");
+        }
+        
+        ArrayList<String> answer = new ArrayList<>();
+        
+        for(HashMap<String, Integer> count: courseMap.values()){
+            count.values()
+                .stream()
+                .max(Comparator.comparingInt(o -> o))
+                .ifPresent(cnt -> count.entrySet()
+                          .stream()
+                          .filter(entry -> cnt.equals(entry.getValue()) && cnt > 1)
+                          .forEach(entry -> answer.add(entry.getKey())));
+        }
+        Collections.sort(answer);
+        return answer.toArray(new String[0]);
+    }
+    
+    public static void combinations(int idx, char[] order, String result){
+        if(courseMap.containsKey(result.length())){
+            HashMap<String, Integer> map = courseMap.get(result.length());
+            map.put(result, map.getOrDefault(result, 0) + 1);
+        }
+        for(int i = idx; i < order.length; i++){
+            combinations(i+1, order, result + order[i]);
+        }
+    }
+}
