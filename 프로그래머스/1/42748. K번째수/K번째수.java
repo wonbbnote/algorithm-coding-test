4a9c566eac9f846c82999b54dc3355318fd449d5
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-09-25 14:56:13 +0900

    [level 1] Title: K번째수, Time: 0.51 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,18 +4,16 @@ class Solution {
         int[] answer = new int[commands.length];
         
         for(int i = 0; i < commands.length; i++){
-            int num = commands[i][1] - commands[i][0] + 1;
-            int start = commands[i][0] - 1;
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int nth = commands[i][2];
             
-            int[] intArr = new int[num];
-            for(int j = 0; j < num; j++){
-                intArr[j] = array[start];
-                start++;
-            }
+            int[] sliceArr = Arrays.copyOfRange(array, start-1, end);
+            Arrays.sort(sliceArr);
+            answer[i] = sliceArr[nth-1];
             
-            Arrays.sort(intArr);
-            answer[i] = intArr[commands[i][2] - 1];
         }
+        
         
         return answer;
     }
