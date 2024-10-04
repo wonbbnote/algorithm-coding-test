894467bbda43ad86d78bb559d1c5550d60147192
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-10-04 20:05:39 +0900

    [level 2] Title: 피보나치 수, Time: 3.42 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -5,19 +5,14 @@ class Solution {
         
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
+        
         dp[0] = 0;
         dp[1] = 1;
         
-        for(int i = 2; i < n+1; i++){
-            if(dp[i] == -1){
-                dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
-            }
-            // System.out.println(Arrays.toString(dp));
+        for(int i = 2; i <= n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
         }
         
-        // answer = dp[n] % 1234567;
-        answer = dp[n];
-        
-        return answer;
+        return dp[n];
     }
 }
