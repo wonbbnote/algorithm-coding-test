cdd1e5e606cf4748da9bf21828e339d5d7dd8f80
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-10-05 21:12:17 +0900

    [level 2] Title: 올바른 괄호, Time: 14.62 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,25 +1,25 @@
 import java.util.*;
 class Solution {
     boolean solution(String s) {
+        boolean answer = true;
+
+        char[] arr = s.toCharArray();
         
-        char[] sArr = s.toCharArray();
-        Stack<String> stack = new Stack<>();
+        ArrayDeque<Character> stack = new ArrayDeque<>();
         
-        for(int i = 0; i < sArr.length; i++){
-            if(sArr[i] == '('){
-                stack.push(")");
-            }else if(sArr[i] == ')' && !stack.isEmpty()){
+        for(char c : arr){
+            if(c == '('){
+                stack.push('(');
+            }else if(!stack.isEmpty()){
                 stack.pop();
             }else{
                 return false;
             }
         }
-        
         if(!stack.isEmpty()){
             return false;
         }
-        
-        return true;
 
+        return answer;
     }
 }
