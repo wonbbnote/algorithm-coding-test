aeb792590534e2cc666bffbd9dccdde5b5f5f947
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-10-05 19:00:30 +0900

    [level 1] Title: 실패율, Time: 305.10 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,24 +1,33 @@
 import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
+        int[] a = new int[N];
         
-        int[] challenger = new int[N+2];
-        for(int i = 0; i < stages.length; i++){
-            challenger[stages[i]] += 1;
-        }
-        
-        HashMap<Integer, Double> fails = new HashMap<>();
-        double total = stages.length;
-        
+        double[][] res = new double[N][2];
+        double denominator = stages.length;
         for(int i = 1; i <= N; i++){
-            if(challenger[i] == 0){
-                fails.put(i, 0.);
-            }else{
-                fails.put(i, challenger[i]/total);
-                total -= challenger[i];
+            int user = 0;
+            for(int j = 0; j < stages.length; j++){
+                if(stages[j] == i){
+                    user++;
+                }
             }
+            res[i-1] = new double[] {i, denominator == 0 ? 0 : (double) user / denominator};
+            // System.out.println("denominator: "+denominator);
+            // System.out.println("user: "+user);
+            denominator -= user;
         }
         
-        return fails.entrySet().stream().sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue())).mapToInt(HashMap.Entry::getKey).toArray();
+        Arrays.sort(res, (o1, o2) -> Double.compare(o2[1], o1[1]));
+        
+        for(int i = 0; i < res.length; i++){
+            System.out.println(Arrays.toString(res[i]));
+        }
+        
+        for(int i = 0; i < N; i++){
+            a[i] = (int) res[i][0];
+        }
+
+        return a;
     }
 }
