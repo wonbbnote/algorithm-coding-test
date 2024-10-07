578d35c9c57d3ffb6df7275ad2d60bc4c4a57894
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-10-07 23:31:42 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 49.52 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = 0; i < arr.length; i++){
+            if(!stack.isEmpty() && stack.peek() == arr[i]){
+                continue;
+            }
+            stack.push(arr[i]);
+        }
+
+        return stack.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
