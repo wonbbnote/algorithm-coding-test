3bcfbc44bc328de21b037c6bad8cf98c92d92f58
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-10-08 23:37:21 +0900

    [level 2] Title: 기능개발, Time: 10.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,32 +1,25 @@
 import java.util.*;
-
-class Solution{
-    public int[] solution(int[] progresses, int[] speeds){
-        Queue<Integer> answer = new ArrayDeque<>(); // 😻 answer을 큐로?
-        int n = progresses.length;
-
-        // 각 작업의 배포 가능일 계산
-        int[] daysLeft = new int[n];
-        for(int i = 0; i < n; i++){
-            daysLeft[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]); // 😻 반올림이 있었군
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        
+        Queue<Integer> queue = new ArrayDeque<>();
+        for(int i = 0; i < progresses.length; i++){
+            int needDays = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+            queue.add(needDays);
         }
-
-        // 배포될 작업의 수 카운트
-        int count = 0;
-        int maxDay = daysLeft[0]; // 현재 배포될 작업 중 가장 늦게 배포될 작업의 가능일
-
-        for(int i = 0; i < n; i++){
-                if(daysLeft[i] <= maxDay){ // 배포 가능일이 가장 늦은 배포일보다 빠르면
-                        count++;
-                }else{ // 배포 가능일이 가장 늦은 배포일보다 느리면, 새로 세팅
-                        answer.add(count);
-                        count = 1;
-                        maxDay = daysLeft[i];
-                }
+        
+        
+        List<Integer> answer = new ArrayList<>();
+        while(!queue.isEmpty()){
+            int num = 1;
+            int first = queue.poll();
+            while(!queue.isEmpty() && queue.peek() <= first){
+                queue.poll();
+                num++;
+            }
+            answer.add(num);
         }
-
-        // 반복문을 나오기 바로 직전에 count한 값은 answer에 add()로 추가하지 않기 때문에 반복문 바깥에서 추가해줘야 한다.
-        answer.add(count);
+          
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
