f287bedc63d0f6f3cacbcacbc0dad444770c44d3
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-10-11 23:33:35 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < clothes.length; i++){
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        
+        Iterator<Integer> iter = map.values().iterator();
+        while (iter.hasNext()) {
+            answer *= iter.next().intValue() + 1;
+        }
+
+        
+        return answer - 1;
+    }
+}
