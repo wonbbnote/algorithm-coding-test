b45266b6322b6f6b2bfc5d638dc6142bdefdeb38
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-10-14 18:21:42 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 35.22 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,38 +1,36 @@
-import java.util.HashMap;
+import java.util.*;
 
-public class Solution {
-
+class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        // ❶ parent 해시맵. key는 enroll의 노드, value는 referral의 노드로 구성됨
-        HashMap<String, String> parent = new HashMap<>();
-        for (int i = 0; i < enroll.length; i++) {
-            parent.put(enroll[i], referral[i]);
-        }
-
-        // ❷ total 해시맵 생성
-        HashMap<String, Integer> total = new HashMap<>();
-
-        // ❸ seller 배열과 amount 배열을 이용하여 이익 분배
-        for (int i = 0; i < seller.length; i++) {
-            String curName = seller[i];
-            // ❹ 판매자가 판매한 총 금액 계산
-            int money = amount[i] * 100;
-            // ❺ 판매자부터 차례대로 상위 노드로 이동하며 이익 분배
-            while (money > 0 && !curName.equals("-")) {
-                // ❻ 현재 판매자가 받을 금액 계산(10%를 제외한 금액)
-                total.put(curName, total.getOrDefault(curName, 0) + money - (money / 10));
-                curName = parent.get(curName);
-                // ❼ 10% 를 제외한 금액 계산
-                money /= 10;
-            }
-        }
-
-        // ❽ enroll 배열의 모든 노드에 대해 해당하는 이익을 배열로 반환
         int[] answer = new int[enroll.length];
-        for (int i = 0; i < enroll.length; i++) {
-            answer[i] = total.getOrDefault(enroll[i], 0);
+        
+        
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < enroll.length; i++){
+            map.put(enroll[i], i);
         }
+        
+        for(int i = 0; i < seller.length; i++){
+            dfs(map.get(seller[i]), (double) amount[i] * 100, map, referral, answer);
+        }
+
         return answer;
     }
-
+    
+    private void dfs(int idx, double money, Map<String, Integer> map, String[] referral, int[] answer){
+        
+        
+        if(money >= 10){
+            double bene = Math.ceil(money * 0.9);
+            answer[idx] += bene;
+            if(!referral[idx].equals("-")){
+                dfs(map.get(referral[idx]), money - bene, map, referral, answer);
+            }
+            
+        }else{
+            answer[idx] += money;
+        }
+        
+        
+    }
 }
