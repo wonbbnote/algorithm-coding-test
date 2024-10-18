d9cec5ca69678916191536e284f00d417b760a74
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-10-18 12:35:38 +0900

    [level 1] Title: 예산, Time: 0.60 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        Arrays.sort(d);
+        
+        for(int value: d){
+            
+            budget -= value;
+            
+            if(budget < 0){
+                break;
+            }
+            
+            answer++;
+            
+        }
+        
+        
+        return answer;
+    }
+}
