d130a4e84c402d935463df9d976ff4a48f169609
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-10-18 13:39:21 +0900

    [level 2] Title: 구명보트, Time: 12.29 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,13 +3,29 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        int index = 0;
+        // System.out.println(Arrays.toString(people));
         
-        for(int i = people.length - 1; i >= index; i--){
-            if(people[i] + people[index] <= limit){
-                index++;
+        int point = people.length - 1;
+        boolean[] boarded = new boolean[people.length];
+        for(int i = 0; i < people.length; i++){
+            
+            if(!boarded[i]){
+                boarded[i] = true;
+            }else{
+                continue;
             }
+            
+            while(point >= 0 && people[i] + people[point] > limit){
+                point--;
+            }
+            
+            if(i < point){
+                boarded[point] = true;
+                point--;
+            }
+            
             answer++;
+            
         }
         
         return answer;
