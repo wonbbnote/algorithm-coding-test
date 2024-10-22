5a260547c52c8357418f1160367f33e958265de8
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-10-23 00:52:39 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.06 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -4,13 +4,12 @@ public class Solution {
     public int solution(int n) {
         int ans = 0;
 
-        while(n > 0){
-            if(n%2 == 1){
-                ans += 1;
-            }
-            n /= 2;
-        }
-
-        return ans;
+        // K 칸을 앞으로 점프 -> K 건전지 사용량
+        // (현재까지 온 거리) x 2 에 해당하는 위치로 순간이동
+        // 점프를 최소한으로 하여 N만큼 떨어져 있는 장소로 가기
+        // 사용해야 하는 건전지 사용량의 최솟값을 return
+    
+        
+        return Integer.toBinaryString(n).replace("0", "").length();
     }
 }
