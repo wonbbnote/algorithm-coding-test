83ea34bbe91c5c22e5c390d920068415c5d13dc2
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-10-23 14:13:53 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.04 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,35 +1,45 @@
 class Solution {
     public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = new int[] {0, 0};
         
-        int max_step_width = (board[0] - 1) / 2;
-        int max_step_height = (board[1] - 1) / 2;
-        int[] loca = new int[] {0,0};
+        int verticalMax = (board[1] - 1) / 2;
+        int verticalMin = 0 - verticalMax;
+        int horizontalMax = (board[0] - 1) / 2;
+        int horizontalMin = 0 - horizontalMax;
         
         for(String key: keyinput){
+            
             switch(key){
-                case "left":
-                    if(loca[0] > -max_step_width){
-                        loca[0]--;
-                    }
-                    break;
-                case "right":
-                    if(loca[0] < max_step_width){
-                        loca[0]++;
-                    }
-                    break;
+                    
                 case "up":
-                    if(loca[1] < max_step_height){
-                        loca[1]++;
+                    if(answer[1] != verticalMax){
+                        answer[1]++;
                     }
                     break;
                 case "down":
-                    if(loca[1] > -max_step_height){
-                        loca[1]--;
+                    if(answer[1] != verticalMin){
+                        answer[1]--;
                     }
                     break;
+                case "left":
+                    if(answer[0] != horizontalMin){
+                        answer[0]--;
+                    }
+                    break;
+                case "right":
+                    if(answer[0] != horizontalMax){
+                        answer[0]++;
+                    }
+                    break;
+                    
+                    
             }
         }
         
-        return loca;
+        
+        
+        
+        
+        return answer;
     }
 }
