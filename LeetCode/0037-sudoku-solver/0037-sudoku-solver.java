60467f29b97a90963843539b4861626cdcffc5ee
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-10-25 18:47:27 +0900

    Time: 8 ms (51.91%), Space: 42.4 MB (8.77%) - LeetHub

--- LeetCode/0037-sudoku-solver/0037-sudoku-solver.java
+++ LeetCode/0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,61 +1,72 @@
 class Solution {
-    public void solveSudoku(char[][] board) {
-        char[][] copy = new char[board.length][];
-        for (int i = 0; i < board.length; i++) {
-            copy[i] = board[i].clone();
-        }
 
-        backtrack(copy, 0);
-
-        for (int i = 0; i < board.length; i++) {
-            board[i] = copy[i].clone();
+    private static class Block{
+        int i, j;
+        public Block(int i, int j){
+            this.i = i;
+            this.j = j;
         }
     }
 
-    boolean backtrack(char[][] board, int n) {
-				//✅ 모든 칸에 올바르게 숫자를 부여했다면 true를 반환한다.
-        if (n == 81) return true;
+    private static char[][] Board;
 
-        int r = n / 9,
-            c = n % 9;
-				//✅ 현재 위치(i, j)가 비어있지 않다면, 다음 위치로 넘어간다. (재귀 함수 호출)
-        if (board[r][c] != '.') return backtrack(board, n+1);
-				//✅ 현재 위치(i, j)가 비어있는지 검사한다.
-        else {
-						//✅ 현재 위치에 1 ~ 9 사이의 정수를 부여해본다.
-            for (int i = 0; i < 9; i++) {
-                char nextValue = (char)(49 + i);
-								//✅ 현재 위치에 선택한 정수의 유효성을 검사한다.
-                if (!isValid(board, r, c, nextValue)) continue;
-	              //✅ board[i][j]에 유효한 정수를 부여한다.
-                board[r][c] = nextValue;
-								//✅ 백트래킹을 수행한다. (재귀 함수 호출)
-                if (backtrack(board, n+1)) return true;
-								//✅ False가 반환되면, board[i][j]를 빈 칸으로 되돌린다.
-                board[r][c] = '.';
+    private static boolean isValid(int num, int row, int col){
+        return !(inRow(num, row) || inCol(num, col) || inBox(num, row, col));
+    }
+
+    private static boolean inRow(int num, int row){
+        for(int i = 0; i < 9;i++){
+            if(Board[row][i] == (char) (num + '0')) return true;
+        }
+        return false;
+    }
+    private static boolean inCol(int num, int col){
+        for(int i = 0; i < 9;i++){
+            if(Board[i][col] == (char) (num + '0')) return true;
+        }
+        return false;
+    }
+    private static boolean inBox(int num, int row, int col){
+        int boxRow = (row / 3) * 3;
+        int boxCol = (col / 3) * 3;
+
+        for(int i = boxRow; i < boxRow + 3; i++){
+            for(int j = boxCol; j < boxCol + 3; j++){
+                if(Board[i][j] == (char) (num + '0')) return true;
             }
         }
-
         return false;
     }
 
-    boolean isValid(char[][] board, int r, int c, char value) {
-        // 행 확인
-        for (int i = 0; i < 9; i++) {
-            if (board[i][c] == value) return false;
-        }
-        // 열 확인
-        for (int i = 0; i < 9; i++) {
-            if (board[r][i] == value) return false;
-        }
-        // 3x3 sub-box 확인
-        int sr = (r / 3) * 3,
-            sc = (c / 3) * 3;
-        for (int dr = 0; dr < 3; dr++) {
-            for (int dc = 0; dc < 3; dc++) {
-                if (board[sr+dr][sc+dc] == value) return false;
+    private static Block findEmptyPosition(){
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(Board[i][j] == '.') return new Block(i, j);
             }
         }
-        return true;
+        return null;
+    }
+
+    private static boolean findSolution(){
+        Block emptyPos = findEmptyPosition();
+        if(emptyPos == null) return true;
+
+        int row = emptyPos.i;
+        int col = emptyPos.j;
+
+        for(int num = 1; num <= 9; num++){
+            if(isValid(num, row, col)){
+                Board[row][col] = (char) (num + '0');
+                if(findSolution()) return true;
+                Board[row][col] = '.';
+            }
+        }
+        return false;
+    }
+
+    public char[][] solveSudoku(char[][] board) {
+        Board = board;
+        findSolution();
+        return board;
     }
 }
