cf375d68a431d3a9022f7d1761f09c5587a5e936
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-10-31 15:52:28 +0900

    [level 2] Title: 삼각 달팽이, Time: 13.54 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,60 +1,39 @@
-import java.util.*;
 class Solution {
-    static int[] answer;
     public int[] solution(int n) {
-
+        int[] answer = new int[n * (n+1) / 2];
+        int[][] matrix = new int[n][n];
         
-        int last = n * (n+1) / 2;
-        answer = new int[last];
+        int num = 1;
+        int x = -1;
+        int y = 0;
         
-        first(1, 0, n, 1);
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(i % 3 == 0){
+                    ++x;
+                }else if(i % 3 == 1){
+                    ++y;
+                }else if(i % 3 == 2){
+                    --x;
+                    --y;
+                }
+                matrix[x][y] = num++;
+            }
+        }
+        
+        int idx = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    break;
+                }
+                answer[idx++] = matrix[i][j];
+            }
+        }
+        
+        
+        
         
         return answer;
     }
-    
-    private static void first(int startNum, int startIdx, int n, int nth){
-        int loopNum = (n - 1) - 3*(nth - 1);
-        if(loopNum == 0){
-            answer[startIdx] = startNum;
-            return;
-        }
-        
-        int added = 2 * (nth - 1) + 1;
-        for(int i = 0; i < loopNum; i++){
-            answer[startIdx] = startNum++;
-            startIdx += added++;
-        }
-        second(startNum, startIdx, n, nth);
-    }
-    
-    private static void second(int startNum, int startIdx, int n, int nth){
-        int loopNum = (n - 1) - 3*(nth - 1);
-        for(int i = 0; i < loopNum; i++){
-            answer[startIdx++] = startNum++;
-        }
-        third(startNum, startIdx, n, nth);
-    }
-    
-    private static void third(int startNum, int startIdx, int n, int nth){
-        int loopNum = (n - 1) - 3*(nth - 1);
-        int minus = n - (nth - 1);
-        for(int i = 0; i < loopNum; i++){
-            answer[startIdx] = startNum++;
-            if(i < loopNum - 1){
-                startIdx -= minus--;
-            }
-            
-        }
-        // System.out.println("last startIdx: "+ startIdx + ", last startNum: "+startNum);
-        // System.out.println("answer: "+ Arrays.toString(answer));
-        
-        if(loopNum >= 3){
-            first(startNum, startIdx + 2 * nth, n, nth+1);
-        }else{
-            return;
-        }
-        
-    }
-    
-    
 }
