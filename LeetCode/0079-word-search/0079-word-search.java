508a6b235a44382e4110b82ec7698df5cf0ca029
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-11-04 18:27:54 +0900

    Time: 218 ms (20.49%), Space: 41.6 MB (40.02%) - LeetHub

--- LeetCode/0079-word-search/0079-word-search.java
+++ LeetCode/0079-word-search/0079-word-search.java
@@ -1,62 +1,51 @@
 class Solution {
-    // -> [0, 1], 아래 [1, 0], 위[-1, 0], <- [0, -1]
+
+    final static int[] dx = {0, 1, 0, -1};
+    final static int[] dy = {-1, 0, 1, 0};
+
+
     public boolean exist(char[][] board, String word) {
+
         for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length;j++){
+            for(int j = 0; j < board[0].length; j++){
                 boolean[][] visited = new boolean[board.length][board[0].length];
-                boolean result = backtracking(i, j, 0, board, word, visited);
-                if(result){
+                visited[i][j] = true;
+                if(search(board, i, j, 0, word, visited)){
                     return true;
                 }
             }
         }
         return false;
+        
     }
 
-    public boolean backtracking(int m, int n, int nth_char, char[][] board, String word, boolean[][] visited){
+    private boolean isValid(char[][] board, int row, int col, boolean[][] visited){
+        return (row >= 0 && row < board.length) && (col >= 0 && col < board[0].length) && !visited[row][col];
+
+    }
+
+    private boolean search(char[][] board, int row, int col, int idx, String word, boolean[][] visited){
         
-        if(word.charAt(nth_char) == board[m][n]){
-            visited[m][n] = true;
+        if(board[row][col] == word.charAt(idx)){
 
-            if(nth_char == word.length()-1){
+            if(idx + 1 == word.length()){
                 return true;
+            }
 
-            }else{
-            
-                if(n < board[0].length - 1 && visited[m][n+1] == false){
-                    if(backtracking(m, n+1, nth_char+1, board, word, visited)){
-                        return true;
-                    }else{
-                        visited[m][n+1] = false;
-                    }; // 오른쪽
-                }
+            for(int i = 0; i < 4; i++){
+                int nextRow = row + dx[i];
+                int nextCol = col + dy[i];
 
-                if(n > 0 && visited[m][n-1] == false){
-                    if(backtracking(m, n-1, nth_char+1, board, word, visited)){
+                if(isValid(board, nextRow, nextCol, visited)){
+                    visited[nextRow][nextCol] = true;
+                    if(search(board, nextRow, nextCol, idx+1, word, visited)){
                         return true;
-                    }else{
-                        visited[m][n-1] = false;
-                    }; // 왼쪽
-                }
-
-                if(m > 0 && visited[m-1][n] == false){
-                    if(backtracking(m-1, n, nth_char+1, board, word, visited)){
-                        return true;
-                    }else{
-                        visited[m-1][n] = false;
-                    }; // 위쪽
-                }
-
-                if(m < board.length - 1 && visited[m+1][n] == false){
-                    if(backtracking(m+1, n, nth_char+1, board, word, visited)){
-                        return true;
-                    }else{
-                        visited[m+1][n] = false;
-                    }; // 아래쪽
+                    }
+                    visited[nextRow][nextCol] = false;
                 }
             }
         }
+
         return false;
-        
     }
 }
