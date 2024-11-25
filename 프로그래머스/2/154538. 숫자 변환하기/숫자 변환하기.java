c6f4b7757d2e82a8c2863a0f88f180d12e147bf8
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-11-25 23:16:14 +0900

    [level 2] Title: 숫자 변환하기, Time: 38.37 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,32 @@
+import java.math.*;
+class Solution {
+    public int solution(int x, int y, int n) {
+        int answer = 0;
+        int[] dp = new int[y+1];
+        
+        for(int i=x; i<=y; i++) {
+            // 변환할수 없는 값이면 -1;
+            if(i!=x && dp[i] == 0) {
+                dp[i] = -1;
+                continue;
+            }
+            
+            // x에 n을 더한 값
+            if(i+n <= y) {
+                dp[i+n] = (dp[i+n] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i+n]);
+            }
+            
+            // x에 2를 곱한 값
+            if(i*2 <= y) {
+                dp[i*2] = (dp[i*2] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i*2]);
+            }
+            
+            // x에 3을 곱한 값
+            if(i*3 <= y) {
+                dp[i*3] = (dp[i*3] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i*3]);
+            }
+        }
+
+        return dp[y];
+    }
+}
