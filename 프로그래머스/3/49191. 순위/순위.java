897fbe221574b60bc248ea977fcde3e508ccde4d
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2024-12-16 16:38:48 +0900

    [level 3] Title: 순위, Time: 21.16 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -6,26 +6,31 @@ class Solution {
         
         for(int i = 0; i < results.length; i++){
             arr[results[i][0]][results[i][1]] = 1;
+            arr[results[i][1]][results[i][0]] = -1;
         }
         
         for(int k = 1; k <= n; k++){
             for(int i = 1; i <= n; i++){
                 for(int j = 1; j <= n; j++){
-                    if(arr[i][k] != 0 & arr[k][j] != 0){
+                    if(arr[i][k] == 1 & arr[k][j] == 1){
                         arr[i][j] = 1;
+                        arr[j][i] = -1;
+                    }else if(arr[i][k] == -1 & arr[k][j] == -1){
+                        arr[i][j] = -1;
+                        arr[j][i] = 1;
                     }
                 }
             }
         }
         
-        for (int i = 1; i <= n; i++) {
-            int game = 0;
-            for (int j = 1; j <= n; j++) {
-                if (arr[i][j] == 1 || arr[j][i] == 1)
-                    game++;
+        for(int i = 1; i <= n; i++){
+            int count = 0;
+            for(int j = 1; j <= n; j++){
+                if(arr[i][j] != 0){ count++; }
             }
-            if (game == n-1)
+            if(count == n-1){
                 answer++;
+            }
         }
         
         
