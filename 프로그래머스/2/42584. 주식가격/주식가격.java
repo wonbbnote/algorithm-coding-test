b4078f6e1db9957a9e56f1b12e637c76ed43e1a5
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2025-01-05 00:06:33 +0900

    [level 2] Title: 주식가격, Time: 27.48 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,10 +2,12 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
+        Arrays.fill(answer, 0);
         
         ArrayDeque<Integer> stack = new ArrayDeque<>();
+        
         for(int i = 0; i < prices.length; i++){
-            while(!stack.isEmpty() && prices[stack.peek()]  > prices[i]){
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
                 int popped = stack.pop();
                 answer[popped] = i - popped;
             }
@@ -16,6 +18,9 @@ class Solution {
             int popped = stack.pop();
             answer[popped] = prices.length - 1 - popped;
         }
+        
+        
+        
         return answer;
     }
 }
