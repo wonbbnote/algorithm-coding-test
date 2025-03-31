18adf645b13fc96dd216ba091d582de5b1058a8b
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2025-03-31 23:13:12 +0900

    [Gold IV] Title: 네트워크 연결, Time: 532 ms, Memory: 50388 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.io.*;
+
+class Edge implements Comparable<Edge> {
+    int src, dest, weight;
+
+    Edge(int src, int dest, int weight) {
+        this.src = src;
+        this.dest = dest;
+        this.weight = weight;
+    }
+
+    public int compareTo(Edge compareEdge) {
+        return this.weight - compareEdge.weight;
+    }
+}
+
+public class Main {
+    static int[] parent;
+
+    static int find(int i) {
+        if (parent[i] == i) {
+            return i;
+        } else {
+            parent[i] = find(parent[i]);
+            return parent[i];
+        }
+    }
+
+    static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            parent[rootY] = rootX;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st;
+
+            int N = Integer.parseInt(br.readLine().trim());
+            int M = Integer.parseInt(br.readLine().trim());
+
+            List<Edge> edges = new ArrayList<>();
+
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine().trim());
+                int A = Integer.parseInt(st.nextToken());
+                int B = Integer.parseInt(st.nextToken());
+                int C = Integer.parseInt(st.nextToken());
+                edges.add(new Edge(A - 1, B - 1, C));
+            }
+
+            // 최소신장트리(MST)는 가중치의 합이 최소가되는 트리로 가중치가 작은 간선부터 연결해요!
+            Collections.sort(edges);
+
+            parent = new int[N];
+            for (int i = 0; i < N; i++) {
+                parent[i] = i;
+            }
+
+            int mstWeight = 0;
+            for (Edge edge : edges) {
+                int x = find(edge.src);
+                int y = find(edge.dest);
+
+                // 만약 간선의 양쪽 노드가 서로다른 집합에 속해 있다면
+                if (x != y) {
+                    union(x, y);
+                    mstWeight += edge.weight;
+                }
+            }
+
+            System.out.println(mstWeight);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
