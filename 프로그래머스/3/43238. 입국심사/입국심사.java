ce96f7bd93ce93f65837d4a1534005ceae024f58
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2025-04-23 11:52:04 +0900

    [level 3] Title: 입국심사, Time: 94.17 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,25 +1,28 @@
 import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
+        long answer = 0;
         
         Arrays.sort(times);
-        long answer = 0;
         long left = 0;
-        long right = times[times.length - 1] * (long)n;
+        long right = times[times.length - 1] * (long) n;
         
         while(left <= right){
-            long mid = (left + right) / 2;
-            long sum = 0;
-            for(int time : times) {
-                sum += mid / time;
+            long mid = ( left + right ) / 2; // n명을 심사하는 데 걸리는 총시간
+            
+            long possibleN = 0;
+            for(int time: times){
+                long cnt = mid / time;
+                possibleN += cnt;
             }
             
-            if(sum < n){
-                left = mid + 1;
+            if(possibleN < n){
+                left = mid + 1; 
             }else{
                 right = mid - 1;
-                answer = mid; 
+                answer = mid;
             }
+            
         }
         
         
