92574824a5885ac297becda9187f9933bacd728d
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2025-07-07 11:43:07 +0900

    [Silver III] Title: N과 M (4), Time: 540 ms, Memory: 27804 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+
+        int[] arr = new int[N];
+        for (int i = 1; i <= N; i++) {
+            arr[i - 1] = i;
+        }
+        backtracking(0, arr, M, new ArrayList<>());
+
+    }
+
+    public static void backtracking(int start, int[] arr, int m, List<Integer> caseList){
+
+        if(caseList.size() == m){
+            for (int i = 0; i < m ; i++) {
+                System.out.print(caseList.get(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = start; i < arr.length; i++) {
+            caseList.add(arr[i]);
+            backtracking(i, arr, m, caseList);
+            caseList.remove(caseList.size() - 1);
+        }
+    }
+}
