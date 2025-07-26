988987fa8e8a6f92a7029a5be16f4440f40e9c12
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2025-07-26 16:43:49 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1560 ms, Memory: 96672 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,62 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer>[] adjList = new ArrayList[N + 1];
+
+        for (int i = 1; i <= N ; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            adjList[u].add(v);
+            adjList[v].add(u);
+        }
+
+        for (int i = 1; i <= N ; i++) {
+            Collections.sort(adjList[i]);
+        }
+
+        int[] res = new int[N+1];
+
+        int nth = 1;
+        boolean[] visited = new boolean[N+1];
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        visited[R] = true;
+        queue.add(R);
+
+        while(!queue.isEmpty()){
+            int curr = queue.poll();
+            res[curr] = nth;
+            nth++;
+
+            for(int next: adjList[curr]){
+                if(!visited[next]){
+                    visited[next] = true;
+                    queue.add(next);
+                }
+            }
+        }
+        
+        for (int i = 1; i <= N ; i++) {
+            System.out.println(res[i]);
+        }
+    }
+}
