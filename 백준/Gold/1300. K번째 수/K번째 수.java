bdf49f067f7789c603df82188e4daedcf9e6fd47
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2025-09-11 13:05:52 +0900

    [Gold I] Title: K번째 수, Time: 140 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+
+        long left = 1;
+        long right = k;
+
+        while(left < right){
+            long mid = ( left + right ) / 2;
+
+            long count = 0;
+            for (int i = 1; i <= N; i++) {
+                count += Math.min(mid / i, N);
+            }
+
+            if(k <= count){
+                right = mid;
+            }else{
+                left = mid + 1;
+            }
+        }
+        System.out.println(left);
+
+    }
+}
