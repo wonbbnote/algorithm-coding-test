dfdd9879c7e8f255ffa3490e4c03129e501f0df9
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2026-09-18 17:55:41 +0900

    Time: 0 ms (100%), Space: 42 MB (70.21%) - LeetHub

---
+++ LeetCode/0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        int[] dp = new int[n + 1];
+        return cal(n, dp);
+    }
+
+    public int cal(int n, int[] dp){
+
+        if(n <= 1){
+            return n;
+        }
+
+        if(dp[n] == 0){
+            dp[n] = cal(n-1, dp) + cal(n-2, dp);
+        }
+
+        return dp[n];
+    }
+}
