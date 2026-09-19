52a2230d88bd123b46d4fd3a7f39269cf4a62213
wonbbnote <102503017+wonbbnote@users.noreply.github.com>
2026-09-19 13:22:27 +0900

    Time: 0 ms (100%), Space: 42.9 MB (19.39%) - LeetHub

--- LeetCode/0198-house-robber/0198-house-robber.java
+++ LeetCode/0198-house-robber/0198-house-robber.java
@@ -1,26 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] memo = new int[nums.length+1];
-        Arrays.fill(memo, -1);
-        return maxMoney(nums, nums.length, memo);
-    }
-
-    public int maxMoney(int[] money_lst, int n, int[] memo){
-
-        if(memo[n] == -1){
-            
-            if(n == 0){
-                memo[0] = 0;
-            }else if(n == 1){
-                memo[1] = money_lst[0];
-            }else{
-                int case_1 = maxMoney(money_lst, n-2, memo) + money_lst[n-1];
-                int case_2 = maxMoney(money_lst, n-1, memo);
-                memo[n] = case_1 >= case_2 ? case_1 : case_2;
-            }
-        }
+        int max = 0;
+        // n번째 집을 털때 갔을 때 최대 털 수 있는 돈
+        // n-2번째를 털고, n번째를 턴다. OR n-1번째 까지 턴다. n번째는 털지 않는다.
+        //dp[2] = Math.max(dp[1], dp[0] + nums[2]);
         
-        return memo[n];
+        if(nums.length <= 1){
+            return nums[0];
+        }
 
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+        max = Math.max(dp[0], dp[1]);
+
+        for(int i = 2; i < nums.length; i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+
+        
     }
 }
